fa42997247ff5a5fd71751b61f2379094e5ed898
chaser-omg <zolagalaxy1790@gmail.com>
2023-09-06 15:13:39 +0300

    Added second function and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,17 @@
 def uppercase_string(input_string):
     return input_string.upper()
+
+
+def capitalize_first_letters(input_string):
+    """
+    Функция, которая делает заглавными первые буквы каждого слова в строке.
+
+    :param input_string: Входная строка
+    :return: Строка с первыми буквами каждого слова в верхнем регистре
+    """
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    capitalized_string = ' '.join(capitalized_words)
+    return capitalized_string
+
+
